3bd4498b57a97bf60df2062ef8d54a8a12b4e8cd
Arnaud Clary <arnaudclary@hotmail.fr>
2023-12-27 10:54:52 +0100

    first commit

---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,18 @@
+import Directory from '../../components/directory/directory-component';
+import { Outlet } from 'react-router-dom';
+
+
+
+const Home = () => {
+
+
+
+  return (
+    <div>
+      <Directory />
+
+     </div>
+  );
+};
+
+export default Home;
